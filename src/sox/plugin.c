98db1661abcba97afce3212b01247aec8c45dde5
Alexander V. Nikolaev <avn@daemon.hole.ru>
2010-09-14 22:13:20 +0000

    Add converting from sox native format to unsigned int for shine

--- src/sox/plugin.c
+++ src/sox/plugin.c
@@ -12,7 +12,7 @@ _fixedlame_write_callback(void *userdata, void *buf, size_t size)
 {
     sox_fixedpoint_mp3_t *self = (sox_fixedpoint_mp3_t *) userdata;
     size_t rc = fwrite(buf, size, 1, self->file);
-    fflush(self->file);
+    //fflush(self->file);
     //fprintf(stderr, "written %d bytes\n", size);
     return rc;
 }
@@ -65,8 +65,43 @@ mp3_stop_write(sox_format_t * ft)
 static size_t
 mp3_write(sox_format_t * ft, const sox_sample_t *buf, size_t samp)
 {
+    int nsamples = samp/ft->signal.channels;
+    int bufsize = nsamples * 4; /* backend accept "forced" stereo of 16bit */
+    uint16_t *buffer = malloc(bufsize);
+    int clips = 0;
+    int idx = 0;
+    int c = 0;
+    int rc;
+    SOX_SAMPLE_LOCALS;
+
+    fprintf(stderr, "Create preprocess buffer: %d\n", bufsize);
+    if(!buffer)
+        return 0;
+
+    if(ft->signal.channels == 1)
+    {
+        /* mono */
+        for(c = 0; c < samp; c++)
+        {
+            /* left channel */
+            buffer[idx++] = SOX_SAMPLE_TO_SIGNED_16BIT(buf[c], clips);
+
+            /* right channel */
+            buffer[idx++] = 0;
+        }
+    }
+    else
+    {
+        /* stereo:  we can ignore special meaning of right channel */
+        for(idx = 0; idx < samp; idx++)
+        {
+           buffer[idx] = SOX_SAMPLE_TO_SIGNED_16BIT(buf[idx], clips);
+        }
+    }
     sox_fixedpoint_mp3_t *self = (sox_fixedpoint_mp3_t *) ft->priv;
-    return fixedlame_encode(self->codec, (void *)buf, samp);
+    rc = fixedlame_encode(self->codec, (void *)buffer, samp);
+    free(buffer);
+    return rc;
 }
 
 const sox_format_handler_t *
